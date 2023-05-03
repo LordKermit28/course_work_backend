f37e51be80a7461c06486cc2dd4950aa147c9815
blau <serzh.plichko@mail.ru>
2023-05-03 17:39:49 +1100

    add test and error catchers in main func

--- utils.py
+++ utils.py
@@ -1,37 +1,59 @@
 import json
 from datetime import datetime
 
-def get_data(file_path):
-    with open(file_path, encoding='utf-8') as f:
-        data = json.load(f)
+def get_data():
+    with open("operations.json", encoding='utf-8') as f:
+        try:
+            data = json.load(f)
+        except json.JSONDecodeError as error:
+            print("Erros of reading file", error)
+            data = None
         return data
 
 def make_str_into_digit(line: str):
-    digit_str = ""
-    for s in line:
-        if s.isdigit():
-            digit_str += s
-    return digit_str
+    try:
+        digit_str = ""
+        for s in line:
+            if s.isdigit():
+                digit_str += s
+        result = digit_str
+    except TypeError as e:
+        print(e)
+        result = None
+    return result
+
+
 
     # Функция для маскирования номера карты
-def mask_card_number(card_number):
+def mask_card_number(card_number: str):
     card_number = make_str_into_digit(card_number)
-    return f"{card_number[:6]} {'*'*4} {'*'*4} {card_number[-4:]}"
+    try:
+        result = f"{card_number[:6]} {'*'*4} {'*'*4} {card_number[-4:]}"
+    except TypeError as e:
+        print(f"Вы должны ввести строковой тип данных\nВы вызвали ошибку {e}")
+        result = None
+    return result
 
     # Функция для маскирования номера счета
-def mask_account(account_number):
-    account_number = make_str_into_digit(account_number)
-    return f"**{account_number[-4:]}"
+def mask_account(account_number: str):
+    try:
+        account_number = make_str_into_digit(account_number)
+        result =  f"**{account_number[-4:]}"
+    except TypeError as e:
+        print(f"Вы должны ввести строковой тип данных\nВы вызвали ошибку {e}")
+        result = None
+    return result
 
 # Сортируем операции по дате (в обратном порядке)
 def sorted_operations(operations):
     return sorted([op for op in operations if op.get('state') == 'EXECUTED'], key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'), reverse=True)
 
-def output_operations(operations):
+def output_operations(operations, n: int):
 
     operations = sorted_operations(operations)
 
-    for operation in operations[:5]:
+    for operation in operations[:n]:
+
         date = datetime.strptime(operation['date'], '%Y-%m-%dT%H:%M:%S.%f').strftime('%d.%m.%Y')
         operation_type = operation['description']
         amount, currency = operation['operationAmount'].values()
@@ -51,6 +73,6 @@ def output_operations(operations):
         print(f"{amount} {currency}")
         print()
 
-data = get_data("operations.json")
-
-output_operations(data)
+# data = get_data()
+#
+# output_operations(data, 5)
